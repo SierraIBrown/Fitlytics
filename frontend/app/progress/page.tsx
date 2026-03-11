e8c72ef5cfd8462d664670720b0f652a87cb2edd
sb255@iastate.edu <sib37451@gmail.com>
2026-03-10 19:13:54 -0500

    Adding in placeholder page for Progress page

---
+++ frontend/app/progress/page.tsx
@@ -0,0 +1,11 @@
+import PageContainer from "../../components/PageContainer";
+
+export default function ProgressPage(){
+    return (
+        <PageContainer title="Progress">
+            <div className="rounded-lg border bg-white p-4">
+                Progress charts will go here.
+            </div>
+        </PageContainer>
+    );
+}
