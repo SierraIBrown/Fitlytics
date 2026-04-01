a7cae7264054c445dfe004de02aba572f717afc5
sb255@iastate.edu <sib37451@gmail.com>
2026-03-31 19:02:53 -0500

    Adding in tests for API Client utility file

---
+++ frontend/app/design/apitest.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { getHealth } from "@/lib/api";
+
+export default function TestApiCall(){
+    useEffect(() => {
+        getHealth()
+            .then((data) => console.log("Health:", data))
+            .catch((err) => console.error("API error:", err));
+    }, []);
+
+    return null;
+}
